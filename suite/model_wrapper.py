f595286b2d026397a36f84ac80b9cd9279a9c8e9
Judith Bernett <bernett@in.tum.de>
2023-12-06 13:39:14 +0100

    Basic environment file, some changes to model wrapper

--- suite/model_wrapper.py
+++ suite/model_wrapper.py
@@ -1,6 +1,8 @@
-from data_wrapper import DrugResponseDataset, DrugFeatureDataset, CellLineFeatureDataset
+from abc import ABC, abstractmethod
+from data_wrapper import DrugResponseDataset, FeatureDataset
 
-class DRPModel(object):
+
+class DRPModel(ABC):
     """
     Abstract wrapper class for drug response prediction models.
     """
@@ -17,52 +19,58 @@ class DRPModel(object):
         self.target = target
         self.build_model(*args, **kwargs)
 
+    @abstractmethod
     def build_model(self, *args, **kwargs):
         """
         Builds the model.
         """
-        raise NotImplementedError("build_model method not implemented")
+        pass
 
-    def train(self, cell_line_input: CellLineFeatureDataset, drug_input: DrugFeatureDataset, output: DrugResponseDataset):
+    @abstractmethod
+    def train(self, cell_line_input: FeatureDataset, drug_input: FeatureDataset, output: DrugResponseDataset):
         """
         Trains the model. Call the respective function from models_code here.
         :param cell_line_input: training data associated with the cell line input
         :param drug_input: training data associated with the drug input
-        :param output: training data associated with the reponse output
+        :param output: training data associated with the response output
         """
-        raise NotImplementedError("train method not implemented")
+        pass
 
-    def predict(self, cell_line_input: CellLineFeatureDataset, drug_input: DrugFeatureDataset):
+    @abstractmethod
+    def predict(self, cell_line_input: FeatureDataset, drug_input: FeatureDataset):
         """
         Predicts the response for the given input. Call the respective function from models_code here.
         :param cell_line_input: input associated with the cell line
         :param drug_input: input associated with the drug
         :return: predicted response
         """
-        raise NotImplementedError("predict method not implemented")
+        pass
 
-    def evaluate(self, cell_line_input: CellLineFeatureDataset, drug_input: DrugFeatureDataset, output: DrugResponseDataset):
+    @abstractmethod
+    def evaluate(self, cell_line_input: FeatureDataset, drug_input: FeatureDataset, output: DrugResponseDataset):
         """
         Evaluates the model. Call the respective function(s) from models_code here.
         :param cell_line_input: evaluation data associated with the cell line input
         :param drug_input: evaluation data associated with the drug input
         :param output: evaluation data associated with the reponse output
         """
-        raise NotImplementedError("evaluate method not implemented")
+        pass
 
+    @abstractmethod
     def save(self, path):
         """
         Saves the model to models_saved.
         :param path: path to save the model
         """
-        raise NotImplementedError("save method not implemented")
+        pass
 
+    @abstractmethod
     def load(self, path):
         """
         Loads the model.
         :param path: path to load the model
         """
-        raise NotImplementedError("load method not implemented")
+        pass
 
     def get_model_name(self):
         """
@@ -82,7 +90,7 @@ class DRPModel(object):
     def get_cell_line_features():
         """
         Transforms the cell line input data into a feature tensor that can be supplied to the train method.
-        :return: CellLineFeatureDataset
+        :return: FeatureDataset
         """
         raise NotImplementedError("get_cell_line_features method not implemented")
 
@@ -90,12 +98,12 @@ class DRPModel(object):
     def get_drug_features():
         """
         Transforms the drug input data into a feature tensor that can be supplied to the train method.
-        :return: DrugFeatureDataset
+        :return: FeatureDataset
         """
         raise NotImplementedError("get_drug_features method not implemented")
 
 
-class SingleDRPModel(DRPModel):
+class SingleDRPModel(DRPModel, ABC):
     """
     Abstract wrapper class for single drug response prediction models.
     """
@@ -110,7 +118,14 @@ class SingleDRPModel(DRPModel):
         """
         super(SingleDRPModel, self).__init__(model_name, target, *args, **kwargs)
 
-    def train(self, cell_line_input: CellLineFeatureDataset, drug_input: str, output: DrugResponseDataset):
+    @abstractmethod
+    def build_model(self, *args, **kwargs):
+        """
+        Builds the model.
+        """
+        pass
+
+    def train(self, cell_line_input: FeatureDataset, drug_input: str, output: DrugResponseDataset):
         """
         Trains the model.
         :param cell_line_input: training data associated with the cell line input
@@ -119,16 +134,17 @@ class SingleDRPModel(DRPModel):
         """
         self.train_drug(cell_line_input, drug_input, output)
 
-    def train_drug(self, cell_line_input: CellLineFeatureDataset, drug_name: str, output: DrugResponseDataset):
+    @abstractmethod
+    def train_drug(self, cell_line_input: FeatureDataset, drug_name: str, output: DrugResponseDataset):
         """
         Trains one model per drug.
         :param cell_line_input: training data associated with the cell line input
         :param drug_name: drug name
         :param output: training data associated with the reponse output
         """
-        raise NotImplementedError("train_drug method not implemented")
+        pass
 
-    def predict(self, cell_line_input: CellLineFeatureDataset, drug_input: str):
+    def predict(self, cell_line_input: FeatureDataset, drug_input: str):
         """
         Predicts the response for the given input.
         :param cell_line_input: input associated with the cell line
@@ -137,7 +153,8 @@ class SingleDRPModel(DRPModel):
         """
         self.predict_drug(cell_line_input, drug_input)
 
-    def predict_drug(self, cell_line_input: CellLineFeatureDataset, drug_name: str):
+    @abstractmethod
+    def predict_drug(self, cell_line_input: FeatureDataset, drug_name: str):
         """
         Predicts the response for the given single drug.
         :param cell_line_input: input associated with the cell line
@@ -146,28 +163,31 @@ class SingleDRPModel(DRPModel):
         """
         raise NotImplementedError("predict_drug method not implemented")
 
-    def evaluate(self, cell_line_input: CellLineFeatureDataset, drug_input: str, output: DrugResponseDataset):
+    @abstractmethod
+    def evaluate(self, cell_line_input: FeatureDataset, drug_input: str, output: DrugResponseDataset):
         """
         Evaluates the model.
         :param cell_line_input: evaluation data associated with the cell line input
         :param drug_input: evaluation data associated with the drug input
-        :param output: evaluation data associated with the reponse output
+        :param output: evaluation data associated with the response output
         """
-        raise NotImplementedError("evaluate method not implemented")
+        pass
 
+    @abstractmethod
     def save(self, path):
         """
         Saves the model to models_saved.
         :param path: path to save the model
         """
-        raise NotImplementedError("save method not implemented")
+        pass
 
+    @abstractmethod
     def load(self, path):
         """
         Loads the model.
         :param path: path to load the model
         """
-        raise NotImplementedError("load method not implemented")
+        pass
 
     @staticmethod
     def get_cell_line_features():
