243c89af5870fd394cb2cb227f685d60f452aeaf
Michael Baggio Lorenz <mlorenz49@gmail.com>
2024-04-02 15:02:48 +0200

    Created random forest regressor model

---
+++ Baselines/random_forest_regressor/model.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+import logging
+import pickle
+import sys
+import warnings
+from os.path import dirname, join, abspath
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import GridSearchCV
+
+sys.path.insert(0, abspath(join(dirname(__file__), '..')))
+from base_model import BaseModel
+from utils.utils import cross_validation_fit
+
+logger = logging.getLogger(__name__)
+
+
+class RandomForestRegression(BaseModel):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.kernel = "linear"
+
+    @property
+    def cell_line_views(self):
+        """
+        Returns the sources the model needs as input for describing the cell line.
+        :return: cell line views, e.g., ["methylation", "gene_expression", "mirna_expression", "mutation"]
+        """
+        return ["gene_expression"]
+
+    @property
+    def drug_views(self):
+        """
+        Returns the sources the model needs as input for describing the drug.
+        :return: drug views, e.g., ["descriptors", "fingerprints", "targets"]
+        """
+        return ["fingerprints"]
+
+    def train(self):
+        """
+        Trains the model for each target in the data dictionary as explained in base_model.py. Hyperparameters are tuned
+        using grid search.
+        """
+        logger.info("Started training models")
+        self.models = {}
+        # sum_all_lengths = 0
+        for target in self.data_dict:
+
+            X_train = self.data_dict.get(target).get("X_train")  # get the training data for the target from dict
+            y_train = self.data_dict.get(target).get("y_train")
+            y_train = y_train.reshape(-1)
+
+            reg = RandomForestRegressor(random_state=0, n_jobs=self.n_cpus)
+
+            # hyperparameter tuning using grid search cross validation
+            model = cross_validation_fit(X_train, y_train, reg, self.nCV_folds, self.hyperparameters)
+
+            self.models[target] = model
+        logger.info("finished training models")
+
+    def save(self, result_path, best_model_dict):
+        """
+        Saves the model parameters and the accuracy metrics to the result path as explained in the base_model.py.
+        """
+        self.models_params = {}
+        for target in best_model_dict["models"]:
+
+            target_model = best_model_dict["models"].get(target)
+            if isinstance(target_model, RandomForestRegressor):
+                self.models_params[target] = {"gini_impurity": target_model.feature_importances_}
+            else:
+                # if model is gridsearchcv, save the best estimator params
+                self.models_params[target] = {"gini_impurity": target_model.best_estimator_.feature_importances_}
+        # save model params dict as pickle
+        with open(result_path + self.task + '_model_params.pkl', 'wb') as f:
+            pickle.dump(self.models_params, f)
+
+        # save accuracy metrics as csv
+        best_model_dict["metric_df"].to_csv(result_path + self.task + '_metrics.csv', index=True)
+        logger.info("saved model parameters as pickle and accuracy metrics as csv")
---
+++ Baselines/random_forest_regressor/test_model.py
@@ -0,0 +1,53 @@
+import logging
+import os
+import sys
+import toml
+from os.path import dirname, join, abspath
+from pathlib import Path
+from sklearn.ensemble import RandomForestRegressor
+
+sys.path.insert(0, abspath(join(dirname(__file__), '..')))
+from model import RandomForestRegression
+from utils.utils import mkdir
+from utils import testing, analysis
+
+# setting up logging
+logging.basicConfig(format='%(asctime)s: %(levelname)s: %(message)s', level=logging.INFO)
+
+# setting up directory for saving results
+# save model parameters and results
+dir_path = "/nfs/home/students/m.lorenz/output/rfr/GDSC/RFR_LDO_2feat/"
+mkdir(dir_path)
+
+# setting up file logging as well
+file_logger = logging.FileHandler(Path(os.getcwd() / Path(dir_path + 'Baseline-models.log')), mode='w')
+file_logger.setLevel(logging.INFO)
+formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+file_logger.setFormatter(formatter)
+logging.getLogger().addHandler(file_logger)
+
+# setting up logger for current module
+logger = logging.getLogger(__name__)
+
+# start logging
+logger.info("Running random forest regression model")
+
+# read in meta data from TOML file
+logger.info("Reading in meta data from TOML file")
+with open('metadata_LDO.toml', 'r') as file:
+    meta_data = toml.load(file)
+
+# create linear regression object
+logger.info("Creating random forest regression object")
+
+rf_regression = testing.parse_data(meta_data, RandomForestRegression)
+
+# perform training, testing and evaluation
+best_models, best_nfeatures, best_scc, best_models_params = (
+    testing.train_test_eval(rf_regression, RandomForestRegressor, "regression", meta_data, dir_path))
+
+# perform data analysis
+logger.info("Performing data analysis")
+analysis.base_analysis(best_models, rf_regression, "regression", meta_data, dir_path)
+analysis.scores_clustering(best_models, dir_path, "regression")
+analysis.scatter_predictions(best_models, dir_path)
