56cffb0085134988dac3b3f88e49103326407286
PascalIversen <p.iversen+1@live.de>
2024-05-15 13:56:18 +0200

    refactoring towards nextflow

---
+++ dreval/response_datasets/gdsc1.py
@@ -0,0 +1,18 @@
+from dreval.dataset import DrugResponseDataset
+import pandas as pd
+
+
+class GDSC1(DrugResponseDataset):
+    """
+    GDSC1 dataset.
+    """
+
+    def __init__(self, path_data: str = "data/", file_name: str = "response_GDSC1.csv"):
+        """
+        :param path: path to the dataset
+        """
+        response_data = pd.read_csv(path_data + f"GDSC/{file_name}")
+        self.response =  response_data["LN_IC50"].values
+        self.cell_line_ids = response_data["CELL_LINE_NAME"].values
+        self.drug_ids = response_data["DRUG_NAME"].values
+        self.predictions = None
--- models/README.md
+++
@@ -1,7 +0,0 @@
-This directory is used for the implementation of the models.
-
-Steps required to implement a new model: 
-1. Create a new file (in a new directory)
-2. Create a class for your model which inherits from the DRPModel class (```from dreval.drp_model import DRPModel```)
-3. Implement the required methods
-4. Add the model to the model factory in ```models/__init__.py```
