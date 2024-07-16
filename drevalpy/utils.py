72bb39dea503d9c07726d9161c80a520a723a846
PascalIversen <p.iversen+1@live.de>
2024-07-16 09:36:12 +0200

    fix partial correlation. returns nan if only one CL or drug and issues a warning

--- drevalpy/utils.py
+++ drevalpy/utils.py
@@ -1,6 +1,6 @@
 import os
 import shutil
-from typing import List, Optional, Tuple, Any
+from typing import List, Optional, Tuple
 import pandas as pd
 from sklearn.model_selection import KFold, GroupKFold
 import numpy as np
@@ -9,8 +9,7 @@ from sklearn.model_selection import train_test_split
 from scipy.stats import pearsonr, spearmanr, kendalltau
 from pingouin import partial_corr
 from sklearn.preprocessing import MinMaxScaler, RobustScaler, StandardScaler
-
-
+import warnings
 def check_arguments(args):
     from drevalpy.models import MODEL_FACTORY
     from drevalpy.datasets import RESPONSE_DATASET_FACTORY
@@ -213,6 +212,7 @@ def leave_group_out_cv(
         cv_sets.append(cv_fold)
     return cv_sets
 
+warning_shown = False
 
 def partial_correlation(
     y_pred: np.ndarray,
@@ -247,8 +247,18 @@ def partial_correlation(
         }
     )
 
+    if (len(df["cell_line_ids"].unique()) < 2) or (len(df["drug_ids"].unique()) < 2):
+        # if we don't have more than one cell line or drug in the data, partial correlation is meaningless
+        global warning_shown
+        if not warning_shown: 
+            warnings.warn("Partial correlation not defined if only one cell line or drug is in the data.")
+            warning_shown=True
+        return (np.nan, np.nan)if return_pvalue else np.nan
+
     df["cell_line_ids"] = pd.factorize(df["cell_line_ids"])[0]
     df["drug_ids"] = pd.factorize(df["drug_ids"])[0]
+    
+
     if df.shape[0] < 3:
         r, p = np.nan, np.nan
     else:
