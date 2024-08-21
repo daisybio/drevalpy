7c6a3b1f86e8b382e181c86d4d59a161c231fa76
PascalIversen <p.iversen+1@live.de>
2024-08-21 16:03:18 +0200

    black

--- tests/test_evaluation.py
+++ tests/test_evaluation.py
@@ -5,7 +5,13 @@ from flaky import flaky
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 
 from drevalpy.datasets.dataset import DrugResponseDataset
-from drevalpy.evaluation import evaluate, pearson, spearman, kendall, partial_correlation
+from drevalpy.evaluation import (
+    evaluate,
+    pearson,
+    spearman,
+    kendall,
+    partial_correlation,
+)
 
 
 def test_evaluate():
@@ -51,6 +57,7 @@ def generate_mock_data():
     drug_ids = np.random.randint(20, 30, size=100).astype(str)
     return response, cell_line_ids, drug_ids
 
+
 @pytest.fixture
 def generate_mock_anticorrelated_data():
     response = np.arange(2e6, 0, -1)
@@ -75,11 +82,9 @@ def generate_mock_correlated_data():
 def test_partial_correlation(generate_mock_data):
     response, cell_line_ids, drug_ids = generate_mock_data
 
-    df = pd.DataFrame({
-        "response": response,
-        "cell_line_id": cell_line_ids,
-        "drug_id": drug_ids
-    })
+    df = pd.DataFrame(
+        {"response": response, "cell_line_id": cell_line_ids, "drug_id": drug_ids}
+    )
 
     df["mean_per_drug"] = df.groupby("drug_id")["response"].transform("mean")
     df["mean_per_cell_line"] = df.groupby("cell_line_id")["response"].transform("mean")
@@ -97,7 +102,6 @@ def test_partial_correlation(generate_mock_data):
 
     pc = partial_correlation(y_pred, response, cell_line_ids, drug_ids)
     assert np.isclose(pc, 0.0, atol=1e-3)
-
 
 
 def test_pearson_correlated(generate_mock_correlated_data):
