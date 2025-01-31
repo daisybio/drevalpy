350119838a7efdbb5b22277e36780b700327607a
PascalIversen <p.iversen+1@live.de>
2025-01-31 14:51:08 +0100

    better late than never

--- drevalpy/experiment.py
+++ drevalpy/experiment.py
@@ -904,7 +904,7 @@ def train_and_predict(
     train_dataset.reduce_to(cell_line_ids=cell_lines_to_keep, drug_ids=drugs_to_keep)
     prediction_dataset.reduce_to(cell_line_ids=cell_lines_to_keep, drug_ids=drugs_to_keep)
     if len(train_dataset) < len_train_before or len(prediction_dataset) < len_pred_before:
-        print(f"Reduced training dataset from {len_train_before} to {len(train_dataset)}, due to of missing features")
+        print(f"Reduced training dataset from {len_train_before} to {len(train_dataset)}, due to missing features")
         print(
             f"Reduced prediction dataset from {len_pred_before} to {len(prediction_dataset)}, due to missing features"
         )
@@ -1143,15 +1143,12 @@ def make_model_list(models: list[type[DRPModel]], response_data: DrugResponseDat
 
 @pipeline_function
 def get_model_name_and_drug_id(model_name: str) -> tuple[str, str | None]:
-    """
-    Get the model name and drug id from the model name.
+    """Get the model name and drug id from the model name.
 
     :param model_name: model name, e.g., SimpleNeuralNetwork or MOLIR.Afatinib
     :returns: tuple of model name and, potentially drug id if it is a single drug model
     :raises AssertionError: if the model name is not found in the model factory
     """
-    print(model_name)
-    print(MULTI_DRUG_MODEL_FACTORY.keys())
     if model_name in MULTI_DRUG_MODEL_FACTORY:
         return model_name, None
     else:
