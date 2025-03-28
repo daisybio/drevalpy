77e2a0a1718b5351d0eeb3f532e331eceaf8c67b
Judith Bernett <bernett@in.tum.de>
2025-03-28 15:29:15 +0100

    adding result path

--- create_report.py
+++ create_report.py
@@ -18,18 +18,19 @@ from drevalpy.visualization import (
 from drevalpy.visualization.utils import parse_results, prep_results, write_results  # , create_html, create_index_html
 
 
-def create_output_directories(custom_id: str) -> None:
+def create_output_directories(result_path: str, custom_id: str) -> None:
     """
     If they do not exist yet, make directories for the visualization files.
 
+    :param result_path: path to the results
     :param custom_id: run id passed via command line
     """
-    os.makedirs(f"results/{custom_id}/violin_plots", exist_ok=True)
-    os.makedirs(f"results/{custom_id}/heatmaps", exist_ok=True)
-    os.makedirs(f"results/{custom_id}/regression_plots", exist_ok=True)
-    os.makedirs(f"results/{custom_id}/corr_comp_scatter", exist_ok=True)
-    os.makedirs(f"results/{custom_id}/html_tables", exist_ok=True)
-    os.makedirs(f"results/{custom_id}/critical_difference_plots", exist_ok=True)
+    os.makedirs(f"{result_path}/{custom_id}/violin_plots", exist_ok=True)
+    os.makedirs(f"{result_path}/{custom_id}/heatmaps", exist_ok=True)
+    os.makedirs(f"{result_path}/{custom_id}/regression_plots", exist_ok=True)
+    os.makedirs(f"{result_path}/{custom_id}/corr_comp_scatter", exist_ok=True)
+    os.makedirs(f"{result_path}/{custom_id}/html_tables", exist_ok=True)
+    os.makedirs(f"{result_path}/{custom_id}/critical_difference_plots", exist_ok=True)
 
 
 def draw_setting_plots(
@@ -41,6 +42,7 @@ def draw_setting_plots(
     custom_id: str,
     dataset: str,
     path_data: pathlib.Path,
+    result_path: str = "results",
 ) -> list[str]:
     """
     Draw all plots for a specific setting (LPO, LCO, LDO).
@@ -53,12 +55,13 @@ def draw_setting_plots(
     :param custom_id: run id passed via command line
     :param dataset: dataset name
     :param path_data: path to the data
+    :param result_path: path to the results
     :returns: list of unique algorithms
     """
     ev_res_subset = ev_res[ev_res["LPO_LCO_LDO"] == lpo_lco_ldo]
     # PIPELINE: SAVE_TABLES
     html_table = HTMLTable(df=ev_res_subset, group_by="all", dataset=dataset, path_data=path_data)
-    html_table.draw_and_save(out_prefix=f"results/{custom_id}/html_tables/", out_suffix=lpo_lco_ldo)
+    html_table.draw_and_save(out_prefix=f"{result_path}/{custom_id}/html_tables/", out_suffix=lpo_lco_ldo)
 
     # only draw figures for 'real' predictions comparing all models
     eval_results_preds = ev_res_subset[ev_res_subset["rand_setting"] == "predictions"]
@@ -66,7 +69,7 @@ def draw_setting_plots(
     # PIPELINE: DRAW_CRITICAL_DIFFERENCE
     cd_plot = CriticalDifferencePlot(eval_results_preds=eval_results_preds, metric="RMSE")
     cd_plot.draw_and_save(
-        out_prefix=f"results/{custom_id}/critical_difference_plots/",
+        out_prefix=f"{result_path}/{custom_id}/critical_difference_plots/",
         out_suffix=lpo_lco_ldo,
     )
 
@@ -98,7 +101,7 @@ def draw_setting_plots(
                     whole_name=False,
                 )
             out_plot.draw_and_save(
-                out_prefix=f"results/{custom_id}/{out_dir}/",
+                out_prefix=f"{result_path}/{custom_id}/{out_dir}/",
                 out_suffix=out_suffix,
             )
 
@@ -125,7 +128,7 @@ def draw_setting_plots(
     # Cross-study evaluation tables
     cross_study_tables = CrossStudyTables(evaluation_metrics=ev_res_subset, path_data=path_data)
     cross_study_tables.draw_and_save(
-        out_prefix=f"results/{custom_id}/html_tables/",
+        out_prefix=f"{result_path}/{custom_id}/html_tables/",
         out_suffix=lpo_lco_ldo,
     )
 
@@ -159,7 +162,7 @@ def draw_per_grouping_setting_plots(
     )
     if corr_comp.name is not None:
         corr_comp.draw_and_save(
-            out_prefix=f"results/{custom_id}/corr_comp_scatter/",
+            out_prefix=f"{result_path}/{custom_id}/corr_comp_scatter/",
             out_suffix=corr_comp.name,
         )
 
@@ -169,7 +172,7 @@ def draw_per_grouping_setting_plots(
         df=evaluation_results_per_group_subs, group_by=grouping, dataset=dataset, path_data=path_data
     )
     html_table.draw_and_save(
-        out_prefix=f"results/{custom_id}/html_tables/",
+        out_prefix=f"{result_path}/{custom_id}/html_tables/",
         out_suffix=f"{grouping}_{lpo_lco_ldo}",
     )
 
@@ -182,6 +185,7 @@ def draw_algorithm_plots(
     t_vs_p: pd.DataFrame,
     lpo_lco_ldo: str,
     custom_id: str,
+    result_path: str = "results",
 ) -> None:
     """
     Draw all plots for a specific algorithm.
@@ -193,6 +197,7 @@ def draw_algorithm_plots(
     :param t_vs_p: true response values vs. predicted response values
     :param lpo_lco_ldo: setting
     :param custom_id: run id passed via command line
+    :param result_path: path to the results
     """
     """
     eval_results_algorithm = ev_res[(ev_res["LPO_LCO_LDO"] == lpo_lco_ldo) & (ev_res["algorithm"] == model)]
@@ -216,7 +221,7 @@ def draw_algorithm_plots(
                 whole_name=True,
             )
         out_plot.draw_and_save(
-            out_prefix=f"results/{custom_id}/{out_dir}/",
+            out_prefix=f"{result_path}/{custom_id}/{out_dir}/",
             out_suffix=f"{model}_{lpo_lco_ldo}",
         )
     """
@@ -273,7 +278,7 @@ def draw_per_grouping_algorithm_plots(
     )
     if corr_comp.name is not None:
         corr_comp.draw_and_save(
-            out_prefix=f"results/{custom_id}/corr_comp_scatter/",
+            out_prefix=f"{result_path}/{custom_id}/corr_comp_scatter/",
             out_suffix=corr_comp.name,
         )
     """
@@ -290,7 +295,7 @@ def draw_per_grouping_algorithm_plots(
             normalize=normalize,
         )
         regr_slider.draw_and_save(
-            out_prefix=f"results/{custom_id}/regression_plots/",
+            out_prefix=f"{result_path}/{custom_id}/regression_plots/",
             out_suffix=f"{name}_{model}",
         )
 
@@ -300,13 +305,16 @@ if __name__ == "__main__":
     parser.add_argument("--run_id", required=True, help="Run ID for the current execution")
     parser.add_argument("--dataset", required=True, help="Dataset name for which to render the result file")
     parser.add_argument("--path_data", required=True, help="Path to the data")
+    parser.add_argument("--result_path", required=False, help="Path to the results, default is ./results")
     args = parser.parse_args()
     run_id = args.run_id
     dataset = args.dataset
     path_data = pathlib.Path(args.path_data).resolve()
+    result_path = args.result_path if args.result_path is not None else "results"
+    result_path = pathlib.Path(result_path).resolve()
     # assert that the run_id folder exists
-    if not os.path.exists(f"results/{run_id}"):
-        raise AssertionError(f"Folder results/{run_id} does not exist. The pipeline has to be run first.")
+    if not os.path.exists(f"{result_path}/{run_id}"):
+        raise AssertionError(f"Folder {result_path}/{run_id} does not exist. The pipeline has to be run first.")
 
     # not part of pipeline
     (
@@ -314,7 +322,7 @@ if __name__ == "__main__":
         evaluation_results_per_drug,
         evaluation_results_per_cell_line,
         true_vs_pred,
-    ) = parse_results(path_to_results=f"results/{run_id}", dataset=dataset)
+    ) = parse_results(path_to_results=f"{result_path}/{run_id}", dataset=dataset)
 
     # part of pipeline: EVALUATE_FINAL, COLLECT_RESULTS
     (
@@ -330,7 +338,7 @@ if __name__ == "__main__":
     )
 
     write_results(
-        path_out=f"results/{run_id}/",
+        path_out=f"{result_path}/{run_id}/",
         eval_results=evaluation_results,
         eval_results_per_drug=evaluation_results_per_drug,
         eval_results_per_cl=evaluation_results_per_cell_line,
@@ -338,11 +346,11 @@ if __name__ == "__main__":
     )
     """
     # For debugging:
-    evaluation_results = pd.read_csv(f"results/{run_id}/evaluation_results.csv", index_col=0)
-    evaluation_results_per_drug = pd.read_csv(f"results/{run_id}/evaluation_results_per_drug.csv", index_col=0)
-    evaluation_results_per_cell_line = pd.read_csv(f"results/{run_id}/evaluation_results_per_cl.csv", index_col=0)
-    true_vs_pred = pd.read_csv(f"results/{run_id}/true_vs_pred.csv", index_col=0)
-    create_output_directories(run_id)
+    evaluation_results = pd.read_csv(f"{result_path}/{run_id}/evaluation_results.csv", index_col=0)
+    evaluation_results_per_drug = pd.read_csv(f"{result_path}/{run_id}/evaluation_results_per_drug.csv", index_col=0)
+    evaluation_results_per_cell_line = pd.read_csv(f"{result_path}/{run_id}/evaluation_results_per_cl.csv", index_col=0)
+    true_vs_pred = pd.read_csv(f"{result_path}/{run_id}/true_vs_pred.csv", index_col=0)
+    create_output_directories(result_path, run_id)
     # Start loop over all settings
     settings = evaluation_results["LPO_LCO_LDO"].unique()
 
@@ -357,6 +365,7 @@ if __name__ == "__main__":
             custom_id=run_id,
             dataset=dataset,
             path_data=path_data,
+            result_path=result_path
         )
         # draw figures for each algorithm with all randomizations etc
         for algorithm in unique_algos:
@@ -368,10 +377,11 @@ if __name__ == "__main__":
                 t_vs_p=true_vs_pred,
                 lpo_lco_ldo=setting,
                 custom_id=run_id,
+                result_path=result_path,
             )
-        # get all html files from results/{run_id}
+        # get all html files from {result_path}/{run_id}
         all_files: list[str] = []
-        for _, _, files in os.walk(f"results/{run_id}"):  # type: ignore[assignment]
+        for _, _, files in os.walk(f"{result_path}/{run_id}"):  # type: ignore[assignment]
             for file in files:
                 if file.endswith("json") or (
                     file.endswith(".html") and file not in ["index.html", "LPO.html", "LCO.html", "LDO.html"]
@@ -382,13 +392,13 @@ if __name__ == "__main__":
             run_id=run_id,
             lpo_lco_ldo=setting,
             files=all_files,
-            prefix_results=f"results/{run_id}",
+            prefix_results=f"{result_path}/{run_id}",
             test_mode=setting,
         )
     # PIPELINE: WRITE_INDEX
     create_index_html(
         custom_id=run_id,
         test_modes=settings,
-        prefix_results=f"results/{run_id}",
+        prefix_results=f"{result_path}/{run_id}",
     )
     """
