98510abb55bea49a8f84b6f42f44f7411af0377a
PascalIversen <p.iversen+1@live.de>
2025-11-03 11:47:46 +0100

    typeguard exclude ray

--- noxfile.py
+++ noxfile.py
@@ -190,7 +190,12 @@ def typeguard(session: Session) -> None:
     """
     session.install(".[fit]")
     session.install("pytest", "typeguard", "pygments")
-    session.run("pytest", f"--typeguard-packages={package}", *session.posargs)
+    session.run(
+        "pytest",
+        f"--typeguard-packages={package}",
+        "--ignore=tests/test_hpam_tune_raytune.py",
+        *session.posargs,
+    )
 
 
 @session(python=python_versions)
