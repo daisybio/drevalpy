e75c7328df8d1d5e412f452afab8399c1e7661da
Gregor-git1 <gregchojetzki@gmail.com>
2026-04-21 03:54:21 +0200

    worked on predict and train, modified build_model

--- drevalpy/models/XGDP/xgdp.py
+++ drevalpy/models/XGDP/xgdp.py
@@ -23,6 +23,9 @@ import torch.nn as nn
 import tqdm
 import utils as u
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn.model_selection import KFold
+from sklearn.model_selection import train_test_split
+from torch.utils.data.dataset import Subset
 from torch.optim import Adam
 from torch.utils.data import Dataset as PytorchDataset
 from torch_geometric.loader import DataLoader
@@ -68,8 +71,8 @@ class XGDP(DRPModel, RegressionMetricsMixin):
 
         self.hyperparameters = hyperparameters
         model_name = hyperparameters.get("model_type", "GATNet")
-
-        self.model = XGDPPredictor(name_hyperparameter=hyperparameter["name_hyperparameter"])
+        #init in train
+        #self.model = XGDPPredictor(name_hyperparameter=hyperparameter["name_hyperparameter"])
         """
          # Log hyperparameters to wandb if enabled
         self.log_hyperparameters(hyperparameters)
@@ -145,185 +148,116 @@ class XGDP(DRPModel, RegressionMetricsMixin):
 
         # step 2: preprocess data
 
+        # step 3: build data loaders + split data
+        #to do create split indices
+        test_data = Subset(data, test_index)
+        train_data = Subset(data, train_index)
+        val_data = Subset(data, val_index)
+        test_loader = DataLoader(test_data, batch_size=test_batch_size, shuffle=False)
+        train_loader = DataLoader(train_data, batch_size=train_batch_size, shuffle=False)
+        val_loader = DataLoader(val_data, batch_size=val_batch_size, shuffle=False)
+
         # step 4: init model + train parameters
         # to do: get feature size from data? ->
+
+        #get model + init
         with_attention = []
         model_name = self.hyperparameters["model"]
         model_class = getattr(models, model_name)
-        # is done after cv split
-        # self.model = model_class() #check hyper parameter for each model
+        self.model = model_class().to(device) #add feature size
         if "GAT" in model_name:
-            return_attention_weights = True
+            self.return_attention_weights = True
         else:
-            return_attention_weights = False
+            self.return_attention_weights = False
+
+        #get model hyperparameters
         n_epochs = self.hyperparameters["n_epochs"]
         lr = self.hyperparameters["lr"]
         train_batch_size = self.hyperparameters["train_batch_size"]
         test_batch_size = self.hyperparameters["test_batch_size"]
         val_batch_size = self.hyperparameters["val_batch_size"]
         device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-        # step 3: build data loaders + split data
-        test_loader = DataLoader(test_data, batch_size=test_batch_size, shuffle=False)
+        optimizer = torch.optim.Adam(self.model.parameters(), lr=lr)
+        do_save = self.hyperparameters["do_save"]
+        
+        #performance 
+        log_interval = 20
+        best_mse = 1000
+        best_pearson = 0
+        best_epoch = -1
+        total_time = 0
+        early_stop_tolerance = 30
+        train_losses = []
+        val_losses = []
+        val_pearsons = []
+        best_ret = []
 
+        
         # step 5: train loop
-        best_model_id = 0
-        best_model = None
-        best_pearson_cv = 0
-        ret_cv = []
-        kf = KFold(n_splits=3)  # creates splits indices for cross validation
-        # does the cv split
-        for i, (train_index, val_index) in enumerate(kf.split(cv_data)):
-            # splits data into val and train according to index form kf
-            train_data = Subset(data, train_index)
-            train_data = Subset(data, val_index)
-            train_loader = DataLoader(train_data, batch_size=train_batch_size, shuffle=False)
-            val_loader = DataLoader(test_data, batch_size=val_batch_size, shuffle=False)
+        for epoch in tqdm(range(n_epochs)):
+            start_time = time.time()
+            train_loss = u.train(self.model, device, train_loader, optimizer, epoch+1, log_interval)
+            G,P = u.predicting(self.model, device, val_loader)
+            ret = [u.rmse(G,P),u.mse(G,P),u.pearson(G,P),u.spearman(G,P),u.coeffi_determ(G,P)]
 
-            # creates model for each cv split
-            model = model_class().to(device)
-            optimizer = torch.optim.Adam(model.parameters(), lr=lr)
-            best_mse = 1000
-            best_pearson = 0
-            best_epoch = -1
-            total_time = 0
-            early_stop_tolerance = 30
-            train_losses = []
-            val_losses = []
-            val_pearsons = []
-            best_ret = []
-            for epoch in tqdm(range(n_epochs)):
-                start_time = time.time()
-                train_loss = train(model, device, train_loader, optimizer, epoch + 1, log_interval)
-                G, P = predicting(model, device, val_loader)
-                ret = [u.rmse(G, P), u.mse(G, P), u.pearson(G, P), u.spearman(G, P), coeffi_determ(G, P)]
-                train_losses.append(train_loss)
-                val_losses.append(ret[1])
-                val_pearsons.append(ret[2])
+            train_losses.append(train_loss)
+            val_losses.append(ret[1])
+            val_pearsons.append(ret[2])
 
-        self.model = best_model
+            if ret[1]<best_mse:
+                #rework to / check which folder is best?
+                #if (do_save): #if enabeled saves best state of the model when model is better than previous versions
+                #    torch.save(self.model.state_dict(), model_folder + model_file_name)
+                    
+                best_epoch = epoch+1
+                best_mse = ret[1]
+                best_pearson = ret[2]
+                best_ret = ret
+
+            total_time += time.time() - start_time
+            if (epoch - best_epoch) > early_stop_tolerance:
+                print('early stop at epoch ', epoch)
+                break
+        # test with the model with best validation performance
+        if self.return_attention_weights:
+            G_test, P_test, attn_weights = u.predicting(self.model, device, test_loader, self.return_attention_weights)
+        else:
+            G_test, P_test = u.predicting(self.model, device, test_loader)
+        #rework 
+        #result_file_name = 'result_' + save_name + '_' + dataset + '_' +  '.csv'
+        #ret_test = [rmse(G_test,P_test),mse(G_test,P_test),pearson(G_test,P_test),spearman(G_test,P_test),coeffi_determ(G_test,P_test)]
+        #if do_save:
+        #    best_model_file_name = 'model_' + save_name + '_' + dataset + '_best' + str(best_model_id) +  '.model'
+        #    torch.save(best_model.state_dict(), model_folder + best_model_file_name)
+        #    if return_attention_weights:
+        #        np.save(br_fol + '/Saliency/AttnWeight/' + model_st + '.npy', attn_weights)
         # step 6: save mdoel (maybe not neccecary beacuse class)
 
-
-class XGDP(DRPModel):
-    # to do
-    def __init__(self):
-        """Initialize the DrugGNN model."""
-        pass
-
-    @classmethod
-    def get_model_name(cls) -> str:
-        """Return the name of the model.
-
-        :return: The name of the model.
-        """
-        return "XGDP"
-
-    @property
-    def cell_line_views(self) -> list[str]:
-        """Return the sources the model needs as input for describing the cell line.
-
-        :return: The sources the model needs as input for describing the cell line.
-        """
-        return ["gene_expression"]
-
-    # to do
-    @property
-    def drug_views(self) -> list[str]:
-        """Return the sources the model needs as input for describing the drug.
-
-        :return: The sources the model needs as input for describing the drug.
-        """
-        return [""]
-
-    def build_model(self, hyperparameters: dict[str, Any]) -> None:
-        """Build the model.
-
-        :param hyperparameters: The hyperparameters.
-        """
-        # Log hyperparameters to wandb if enabled
-        self.log_hyperparameters(hyperparameters)
-
-        self.hyperparameters = hyperparameters
-        # init model in train
-
-    def train(
+def predict(
         self,
-        output: DrugResponseDataset,
+        cell_line_ids: np.ndarray,
+        drug_ids: np.ndarray,
         cell_line_input: FeatureDataset,
         drug_input: FeatureDataset | None = None,
-        output_earlystopping: DrugResponseDataset | None = None,
-        **kwargs,
-    ):
-        """Train the model.
-
-        :param output: The output dataset.
-        :param cell_line_input: The cell line input dataset.
-        :param drug_input: The drug input dataset.
-        :param output_earlystopping: The early stopping output dataset.
-        :param kwargs: Additional arguments.
-        :raises ValueError: If drug input is not provided.
+    ) -> np.ndarray:
         """
-        if drug_input is None:
-            raise ValueError("Drug input is required for XGDP")
+        Predicts the response for the given input.
 
-        # step1 load data
+        :param drug_ids: list of drug ids, also used for single drug models, there it is just an array containing the
+            same drug id
+        :param cell_line_ids: list of cell line ids
+        :param cell_line_input: input associated with the cell line, required for all models
+        :param drug_input: input associated with the drug, optional because single drug models do not use drug features
+        :returns: predicted response
+        """
+        #step 1 load data
+        data = None
+        #(step 2 preprocess)
 
-        # step 2: preprocess data
-
-        # step 4: init model + train parameters
-        # to do: get feature size from data? ->
-        with_attention = []
-        model_name = self.hyperparameters["model"]
-        model_class = getattr(models, model_name)
-        # is done after cv split
-        # self.model = model_class() #check hyper parameter for each model
-        if "GAT" in model_name:
-            return_attention_weights = True
-        else:
-            return_attention_weights = False
-        n_epochs = self.hyperparameters["n_epochs"]
-        lr = self.hyperparameters["lr"]
-        train_batch_size = self.hyperparameters["train_batch_size"]
-        test_batch_size = self.hyperparameters["test_batch_size"]
-        val_batch_size = self.hyperparameters["val_batch_size"]
+        #step 3 dataloaders + 
+        loader = DataLoader(data, batch_size=1, shuffle=False)
         device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-        # step 3: build data loaders + split data
-        test_loader = DataLoader(test_data, batch_size=test_batch_size, shuffle=False)
-
-        # step 5: train loop
-        best_model_id = 0
-        best_model = None
-        best_pearson_cv = 0
-        ret_cv = []
-        kf = KFold(n_splits=3)  # creates splits indices for cross validation
-        # does the cv split
-        for i, (train_index, val_index) in enumerate(kf.split(cv_data)):
-            # splits data into val and train according to index form kf
-            train_data = Subset(data, train_index)
-            train_data = Subset(data, val_index)
-            train_loader = DataLoader(train_data, batch_size=train_batch_size, shuffle=False)
-            val_loader = DataLoader(test_data, batch_size=val_batch_size, shuffle=False)
-
-            # creates model for each cv split
-            model = model_class().to(device)
-            optimizer = torch.optim.Adam(model.parameters(), lr=lr)
-            best_mse = 1000
-            best_pearson = 0
-            best_epoch = -1
-            total_time = 0
-            early_stop_tolerance = 30
-            train_losses = []
-            val_losses = []
-            val_pearsons = []
-            best_ret = []
-            for epoch in tqdm(range(n_epochs)):
-                start_time = time.time()
-                train_loss = train(model, device, train_loader, optimizer, epoch + 1, log_interval)
-                G, P = predicting(model, device, val_loader)
-                ret = [u.rmse(G, P), u.mse(G, P), u.pearson(G, P), u.spearman(G, P), coeffi_determ(G, P)]
-                train_losses.append(train_loss)
-                val_losses.append(ret[1])
-                val_pearsons.append(ret[2])
-
-        self.model = best_model
-        # step 6: save mdoel (maybe not neccecary beacuse class)
+        #step 4 predict
+        ret_att = self.return_attention_weights
+        pred = u.predicting(model = self.model,device = device,loader = loader, return_attention_weights= ret_att)
+        #step 5 convert to dreval datatype + return
