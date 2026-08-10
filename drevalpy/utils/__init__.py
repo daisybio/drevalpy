de1d4f21dc6da948b7674e6f268ee6e526bdd9b7
Nico Trummer <nictru32@gmail.com>
2026-08-10 13:53:51 +0200

    chore: remove legacy utils/pipeline.py (superseded by drevalpy.run)

--- drevalpy/utils/__init__.py
+++ drevalpy/utils/__init__.py
@@ -1,10 +1,9 @@
-"""Pipeline helpers, argument checks, response transforms, and decorators."""
+"""Utility helpers: argument checks, response transforms, and decorators."""
 
 from __future__ import annotations
 
 from ._pipeline_function import pipeline_function
 from .checkpoints import TEMPORARY_CHECKPOINT_DIR, checkpoint_dir_or_temporary, resolve_checkpoint_dir
-from .pipeline import main
 from .response_transform import get_response_transformation
 from .validation import check_arguments
 
@@ -13,7 +12,6 @@ __all__ = [
     "check_arguments",
     "checkpoint_dir_or_temporary",
     "get_response_transformation",
-    "main",
     "pipeline_function",
     "resolve_checkpoint_dir",
 ]
